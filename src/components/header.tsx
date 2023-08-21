ded3a97c59c4b60d7ba41cacf655df4f598d5c66
gabriel.sousa <gabriel.sousa@wansoft.com.br>
2023-08-21 10:15:53 -0300

    estudando dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,26 @@
-import {MoonIcon} from "@heroicons/react/24/solid"
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid"
 import React from 'react'
+import ThemeFile from "./themeFile"
 
-//Para usar responsividade em max-width e min-width utilizar o seguinte utilitários max-[] ou min-[]
+//responsividade em max-width e min-width utilizar o seguinte utilitários max-[] ou min-[]
 
 export const Header = () => {
     return (
-        <nav className="bg-blue-400 flex justify-between items-center h-20 px-10 flex-wrap max-[500px]:justify-center">
-            <h2 className='font-bold text-white text-2xl'>
+        <nav className=" bg-slate-50 dark:bg-black flex justify-between items-center h-20 px-10 flex-wrap max-[500px]:justify-center">
+            <h2 className='font-bold text-black text-2xl dark:text-white'>
                 Talwindcss
             </h2>
             <ul className='list-none flex space-x-2'>
-                <li className='font-normal text-lg  text-white'>
+                <li className='font-normal text-lg  text-black dark:text-white'>
                     React
-                </li> 
-                <li className='font-normal text-lg text-white'>
+                </li>
+                <li className='font-normal text-lg text-black dark:text-white'>
                     Next JS
                 </li>
-                <li className='font-normal text-lg text-white'>
+                <li className='font-normal text-lg text-black dark:text-white'>
                     React Native
-                </li> 
-                <button>
-                  <MoonIcon  className="h-6 w-6"/>
-                </button>
+                </li>
+               <ThemeFile />
             </ul>
         </nav>
     )
---
+++ src/components/themeFile/index.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+
+// para implementar dark mode aplicação deve utilizar a classe utilitária dark e aplicar no body do html de acordo com as cores  
+// systemPreference verificar o tema padrão da maquina
+
+export default function ThemeFile() {
+    const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const toogle = () => document.documentElement.classList.toggle('dark')
+
+
+    useEffect(() => {
+        systemPreference && document.documentElement.classList.add('dark')
+    }, [])
+
+    return (
+        <div className="flex">
+            <MoonIcon className="h-6 w-6 text-black dark:text-white dark:hidden cursor-pointer" onClick={toogle} />
+            <SunIcon className="h-6 w-6 text-black hidden dark:text-white dark:block cursor-pointer" onClick={toogle} />
+        </div>
+    )
+}
