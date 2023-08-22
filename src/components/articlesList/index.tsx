a1d42ce4eb8408427e2d8c95eed78faee2287e89
gabriel.sousa <gabriel.sousa@wansoft.com.br>
2023-08-22 13:52:50 -0300

    implementando acessiblidade

--- src/components/articlesList/index.tsx
+++ src/components/articlesList/index.tsx
@@ -5,7 +5,10 @@ const ArticleItem = ({ text, img }: { text: string, img: string }) => {
     return (
         <div className="bg-slate-200  rounded px-10 py-10 flex flex-col items-center justify-center">
             <p className="font-normal text-base text-black text-left mb-5">{text}</p>
-            <img src={img} alt='imagem' />
+            <img src={img} alt='imagem' /> 
+            {
+              img && <span className="sr-only">video sobre alura</span>  
+            }
         </div>
     )
 }
