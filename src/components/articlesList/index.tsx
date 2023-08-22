6a2317ea90d280da8fda91d362ee8e37f4e6cfd7
gabriel.sousa <gabriel.sousa@wansoft.com.br>
2023-08-22 14:01:42 -0300

    mudando cores

--- src/components/articlesList/index.tsx
+++ src/components/articlesList/index.tsx
@@ -3,8 +3,8 @@ import { articlesItems } from "../../constants/articles"
 
 const ArticleItem = ({ text, img }: { text: string, img: string }) => {
     return (
-        <div className="bg-slate-200  rounded px-10 py-10 flex flex-col items-center justify-center">
-            <p className="font-normal text-base text-black text-left mb-5">{text}</p>
+        <div className="bg-slate-200 dark:bg-dark-200  rounded px-10 py-10 flex flex-col items-center justify-center">
+            <p className="font-normal text-base text-black dark:text-gray-400 text-left mb-5">{text}</p>
             <img src={img} alt='imagem' /> 
             {
               img && <span className="sr-only">video sobre alura</span>  
