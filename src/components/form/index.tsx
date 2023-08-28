fc294e82ef7234f8ae08468a3880fe3e698e892b
gabriel.sousa <gabriel.sousa@wansoft.com.br>
2023-08-28 14:45:00 -0300

    aprendendo sobre animações talwindCss

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, useContext } from "react"
 import { GlobalContext } from "../../context/GlobalContext"
+import { Input } from "../../components"
 
 const Form = () => {
 
@@ -13,13 +14,13 @@ const Form = () => {
         if (user.email && user.name) {
             setAuth(true)
         }
-    } 
+    }
 
     return (
-        <form onSubmit={handleSubmit} className={`${auth && 'hidden'}`}>
-            <input name='name' placeholder="nome" value={user.name} onChange={handleChangeInput} />
-            <input name='email' placeholder="nome" value={user.email} onChange={handleChangeInput} />
-            <button type="submit">Enviar</button>
+        <form onSubmit={handleSubmit} className={`${auth && 'hidden'} flex flex-col justify-center items-center h-screen px-5`}>
+            <Input name='name' placeholder="nome" value={user.name} onChange={handleChangeInput} />
+            <Input name='email' placeholder="E-mail" value={user.email} onChange={handleChangeInput} />
+            <button type="submit" className="w-full max-w-sm bg-blue-400 text-white h-10 rounded-full mt-5 hover:animate-pulse">Enviar</button>
         </form>
     )
 
